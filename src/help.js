9fe8c714550bbf269cec0547baed2ee48760377e
Rodrigo631 <76740774+Rodrigo631@users.noreply.github.com>
2021-01-04 20:46:44 +0000

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,57 @@
+const help = (prefix, pushname) => {
+	return `✪═⟪ MAGICAL BOT DOMINA ⟫═✪
+
+*CREADOR : RODRIGO MAGICAL*
+*BOT TYPE : MAGICAL DOMINHA*
+
+╔════════════════════
+║ Hai Kak *${pushname}*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│1. ${prefix}bugreport <lapor bug>
+║│2. ${prefix}runtime
+║│3. ${prefix}join <linkgroup>
+║│4. &{prefix}welcome <1 ou 0>
+║╰───────────
+║╭──❉ *GROUP MENU* ❉──
+║│1. ${prefix}clone
+║│2. ${prefix}promote <@tagmember>
+║│3. ${prefix}demote <@tagadmin>
+║│4. ${prefix}tagall <1 atau 2 atau 3>
+║│5. ${prefix}simih <0 atau 1>
+║│6. ${prefix}group <open atau close>
+║│8. ${prefix}setdesc <teks>
+║│9. ${prefix}setpp 
+║│10. ${prefix}setname <teks>
+║│11. ${prefix}kick <@tagmember>
+║│12. ${prefix}linkgroup
+║╰───────────
+║╭──❉ *MEDIA* ❉──
+║│1. ${prefix}toimg <reply stiker>
+║│2. ${prefix}sticker
+║│3. ${prefix}ttp <teks>
+║│4. ${prefix}sticker nobg <ERROR>
+║│5. ${prefix}tts <kode bahasa> <teks>
+║│6. ${prefix}url2img <tipe> <url>
+║│7. ${prefix}wait <kirim atau reply foto>
+║│8. ${prefix}ocr
+║│9. ${prefix}nulis <teks>
+║╰───────────
+║╭──❉ *OWNER MENU* ❉──
+║│1. ${prefix}setprefix <prefix>
+║│2. ${prefix}bc <promosi>
+║│3. ${prefix}setppbot 
+║│4. ${prefix}clone @tagmember
+║╰───────────
+║╭──❉ *IKLAN* ❉──
+║│1. Instagram : BREVEMENTE
+║│2. Criador do bot
+║│ https://wa.me/+351928044353
+║╰───────────
+║╭──❉ *PERATURAN* ❉──
+║│1. RESPEITE ADMS
+║│2. PROIBIDO SPAM
+║╰───────────
+╠════════════════════
+║  _*MAGICA DOMINA PORRA ⚡*_
+╚══════════════════
